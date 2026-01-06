f574436569ab03624c80174856d36598728c40fc
ireshdilsh <ireshdilshan46@gmail.com>
2026-01-06 13:38:08 +0530

    initial commit

---
+++ src/config/dbConfig.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const connectDB = async() => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_DB_URL as string);
+        console.log('Database connected successfully', conn.connection.host);
+    } catch (error) {
+        console.error('Database connection failed:', error);
+    }
+}
+
+export default connectDB;
---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from 'cors';
+import connectDB from "./config/dbConfig";
+
+const server = express();
+
+server.use(cors())
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+
+const PORT = 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+connectDB()
