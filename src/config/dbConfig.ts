8d7259e02372b6ee3fb1391acbdc9e51718d4f29
ireshdilsh <ireshdilshan46@gmail.com>
2026-01-06 15:00:03 +0530

    fix: simplify database connection logging in connectDB function

--- src/config/dbConfig.ts
+++ src/config/dbConfig.ts
@@ -4,8 +4,8 @@ dotenv.config();
 
 export const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_DB_URL as string);
-        console.log('Database connected successfully', conn.connection.host);
+        await mongoose.connect(process.env.MONGO_DB_URL as string);
+        console.log('Database connected successfully');
     } catch (error) {
         console.error('Database connection failed:', error);
     }
