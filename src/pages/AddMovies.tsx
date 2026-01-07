a97ee626308805425a49b21294092ede20530490
ireshdilsh <ireshdilshan46@gmail.com>
2026-01-07 10:58:47 +0530

    feat: enhance AddMovies component with detailed input fields for movie details

--- src/pages/AddMovies.tsx
+++ src/pages/AddMovies.tsx
@@ -23,9 +23,51 @@ export default function AddMovies() {
       </div>
 
       {/* add movie form */}
-      <div>
+      <div className='w-full flex justify-center items-center flex-col mt-10'>
+        
+        <div className='flex justify-center items-center gap-5'>
+          <div className='flex flex-col justify-start items-start'>
+            <p className='text-gray-800 font-medium text-sm'>Movie Name</p>
+            <input type="text" className='h-11 mt-1 w-100 border px-4 border-neutral-200 rounded-sm' />
+          </div>
+          <div className='flex flex-col justify-start items-start'>
+            <p className='text-gray-800 font-medium text-sm'>Description</p>
+            <input type="text" className='h-11 mt-1 w-100 border px-4 border-neutral-200 rounded-sm' />
+          </div>
+        </div>
+
+        <div className='flex flex-col items-start justify-start'>
+          <p className='text-gray-800 font-medium text-sm mt-4'>Description</p>
+          <textarea name="" id="" className='w-205 h-40 border px-4 py-2 border-neutral-200 rounded-sm mt-1'></textarea>
+        </div>
+
+         <div className='flex justify-center items-center gap-5 mt-5'>
+          <div className='flex flex-col justify-start items-start'>
+            <p className='text-gray-800 font-medium text-sm'>Price</p>
+            <input type="text" className='h-11 mt-1 w-100 border px-4 border-neutral-200 rounded-sm' />
+          </div>
+          <div className='flex flex-col justify-start items-start'>
+            <p className='text-gray-800 font-medium text-sm'>Genre</p>
+            <select name="" id="" className='w-100 h-11 mt-1 border border-neutral-200 rounded-sm px-4'>
+              <option value="action">Action</option>
+              <option value="comedy">Comedy</option>
+              <option value="drama">Drama</option>
+              <option value="horror">Horror</option>
+              <option value="sci-fi">Sci-Fi</option>
+            </select>
+          </div>
+        </div>
+
+        <div className='flex flex-col justify-start items-start mt-5'>
+            <p className='text-sm text-gray-800 font-medium'>Movie Banner</p>
+            <button className='flex-col w-205 h-50 cursor-pointer bg-neutral-50 rounded-md mt-1 text-gray-400 border border-dashed border-neutral-300 flex justify-center items-center gap-2'>
+              <img src="https://img.icons8.com/?size=100&id=1501&format=png&color=999999" className='h-15' alt="add-mark" />
+              <p className='text-lg text-neutral-400'>Add Movie Banner</p>
+            </button>
+        </div>
 
       </div>
     </div>
   )
 }
+ 
