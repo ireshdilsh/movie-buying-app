c91d081c7e634a8bc3f13b42534dee710fdde61e
ireshdilsh <ireshdilshan46@gmail.com>
2026-01-08 22:53:02 +0530

    fix: remove unused import statements in movie.route.ts for cleaner code

--- src/routes/movie.route.ts
+++ src/routes/movie.route.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import { upload } from '../middlewares/upload';
-import { getMovies, saveMovie } from '../controllers/movie.controller';
+import { getMovieById, getMovies, saveMovie } from '../controllers/movie.controller';
 
 export const movie_router = express.Router();
 
 movie_router.post('/post/new/movie',upload.single('image'),saveMovie)
 movie_router.get('/get/movies',getMovies)
+movie_router.get('/get/movie/:id',getMovieById)
