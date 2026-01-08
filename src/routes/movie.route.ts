5821577ef9ed54c01d6dd12fa232dcae86f51785
ireshdilsh <ireshdilshan46@gmail.com>
2026-01-09 00:21:21 +0530

    feat: add deleteMovie endpoint to movie router for movie deletion functionality

--- src/routes/movie.route.ts
+++ src/routes/movie.route.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { upload } from '../middlewares/upload';
-import { getMovieById, getMovies, saveMovie } from '../controllers/movie.controller';
+import { deleteMovie, getMovieById, getMovies, saveMovie } from '../controllers/movie.controller';
 
 export const movie_router = express.Router();
 
 movie_router.post('/post/new/movie',upload.single('image'),saveMovie)
 movie_router.get('/get/movies',getMovies)
 movie_router.get('/get/movie/:id',getMovieById)
+movie_router.delete('/delete/movie/:id',deleteMovie)
