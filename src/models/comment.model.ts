ac1b5aed1d615c7a7f6e22401aac428721ea76fc
ireshdilsh <ireshdilshan46@gmail.com>
2026-01-09 19:52:18 +0530

    feat: add comment model, controller, and router for comment functionality

---
+++ src/models/comment.model.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    movieID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Movie",
+        required: true
+    }
+}, { timestamps: true });
+
+export const Comment = mongoose.model("Comment", commentSchema);
---
+++ src/routes/comment.route.ts
@@ -0,0 +1,3 @@
+import express from 'express';
+
+export const comment_router = express.Router();
