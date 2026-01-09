80f5ecc8895ca898360eed67c2ac1c97779e2ef0
ireshdilsh <ireshdilshan46@gmail.com>
2026-01-09 19:39:08 +0530

    feat: enhance UserMovieByID layout and improve movie details display

--- src/component/UserNavbar.tsx
+++ src/component/UserNavbar.tsx
@@ -14,7 +14,7 @@ export default function UserNavbar() {
     return (
         <div>
             <nav className="flex justify-between items-center w-full mt-5 px-5 sm:px-8 md:px-12 lg:px-20 border-b border-b-neutral-100 pb-3">
-                <img onClick={gotoUserDashboard} src={dark_logo} className="h-6 sm:h-7" alt="Dark-Logo" />
+                <img onClick={gotoUserDashboard} src={dark_logo} className="h-6 cursor-pointer sm:h-7" alt="Dark-Logo" />
 
                 {/* Desktop Menu */}
                 <div className=" sm:flex justify-center items-center gap-4 md:gap-6">
--- src/pages/UserMovieByID.tsx
+++ src/pages/UserMovieByID.tsx
@@ -24,49 +24,35 @@ export default function UserMovieByID() {
     return (
         <div className='flex flex-col px-4 sm:px-6'>
             <UserNavbar />
-            <div className='flex justify-center items-center flex-col mt-5'>
-                <div className='flex flex-start items-start flex-col 
-                    w-full max-w-5xl'>
-
-                    <div className='border flex justify-center items-center gap-3 
-                      border-neutral-200 px-4 py-0.5 mt-6 sm:mt-10 
-                      rounded-2xl self-start'>
-                        <h2 className='text-sm text-neutral-500'>{movie?.genre}</h2>
-                        <div className='bg-amber-500 h-2 w-2 rounded-full'></div>
+            <div className='flex justify-center items-center flex-col mt-20'>
+                <div className='flex justify-center gap-10'>
+                    <img src={movie?.bannerURL} className='object-cover h-90 rounded-lg' alt="" />
+                    <div className='flex justify-start items-start flex-col'>
+                        <p className='border border-neutral-200 px-4 py-1 text-sm text-gray-600 rounded-2xl font-medium'>{movie?.genre}</p>
+                        <h1 className='text-4xl font-medium w-74'>{movie?.name}</h1>
+                        <div className='flex justify-center items-center gap-3 mt-5'>
+                            <p className='px-4 py-1.5 bg-black rounded-sm text-white text-sm'>Trailer</p>
+                            <p className='px-4 py-1.5 border rounded-sm text-sm'>WEBDL</p>
+                            <p className='px-4 py-1.5 bg-red-500 rounded-sm text-white text-sm'>IMDB 7.3</p>
+                        </div>
+                        <p className='flex justify-center items-center text-neutral-500 gap-2.5 text-sm mt-5'>
+                            Director:<p className='font-medium text-black'>{movie?.director}</p>
+                        </p>
+                        <p className='flex justify-center items-center text-neutral-500 gap-2.5 text-sm mt-1'>
+                            Price:<p className='font-medium text-black'>Rs.{movie?.price}/=</p>
+                        </p>
+                        <button className='bg-amber-500 mt-3 px-4 py-1.5 rounded-sm cursor-pointer flex font-medium justify-center items-center gap-2.5 hover:opacity-80'>Buy Now
+                            <img src="https://img.icons8.com/?size=100&id=84998&format=png&color=000000" className='h-5' alt="" />
+                        </button>
                     </div>
-
-                    <h2 className='text-2xl sm:text-3xl font-bold mt-1'>
-                        {movie?.name}
-                    </h2>
-
-                    {/* Responsive image */}
-                    {movie?.bannerURL && (
-                        <img
-                            src={movie.bannerURL}
-                            alt="banner"
-                            className='my-5 sm:my-7 w-full max-h-[450px] object-cover'
-                        />
-                    )}
-
-                    <p className='text-gray-600 font-medium mt-2 text-justify text-sm sm:text-base'>
-                        {movie?.description}
-                    </p>
-
-                    {/* Responsive bottom section */}
-                    <div className='flex flex-col sm:flex-row 
-                      justify-between items-start sm:items-center 
-                      mt-5 w-full gap-3 my-5'>
-                        <h2 className='text-gray-900 font-medium border rounded-sm 
-                      border-neutral-200 px-4 py-1.5 text-sm sm:text-base'>
-                            Directors is {movie?.director}
-                        </h2>
-
-                        <h2 className='text-gray-900 font-medium bg-amber-500 
-                      px-4 py-2 rounded-sm text-sm sm:text-base'>
-                            Rs.{movie?.price}.00
-                        </h2>
+                </div>
+                <p className='w-245 mt-10 text-justify border-b border-b-neutral-300 pb-7'>{movie?.description}</p>
+                <div className='w-245 mt-5 flex flex-col mb-8'>
+                    <h2 className='text-3xl font-medium'>Comments</h2>
+                    <div className='flex justify-center items-center gap-2 mt-5'>
+                        <input type="text" className='bg-neutral-100 px-4 h-10 w-full' placeholder='Write your comment' />
+                        <button className='bg-black flex justify-center items-center h-10 rounded-sm cursor-pointer hover:opacity-80'><img className='h-6 px-4' src="https://img.icons8.com/?size=100&id=bmqaSU1DN4QP&format=png&color=ffffff" alt="" /></button>
                     </div>
-
                 </div>
             </div>
         </div>
